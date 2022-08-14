9b8dcafa5f924053be1b5ee77f5d606cf0c52efe
Dynylson <dynylsonjr@gmail.com>
2022-08-14 18:20:05 -0300

    API route adicionada

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
@@ -39,7 +39,7 @@ const Home = ({ product }: HomeProps) => {
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1LGxAYECxCKV3EN4DUN9Qthr");
 
   const product = {
@@ -54,6 +54,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
 
