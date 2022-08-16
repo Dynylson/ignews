f4d47d71e73a57344220bf68b6bdcc03bd1fc243
Dynylson <dynylsonjr@gmail.com>
2022-08-15 21:21:07 -0300

    Autenticação com GitHub

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,22 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, useSession, signOut } from "next-auth/react";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
-  return isUserLoggedIn ? (
-    <button className={styles.signInButton} type="button">
+  console.log(session);
+
+  return session ? (
+    <button className={styles.signInButton} type="button" onClick={() => signOut()}>
       <FaGithub color="#04d361" />
-      Dynylson Júnior
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button className={styles.signInButton} type="button">
+    <button className={styles.signInButton} type="button" onClick={() => signIn("github")}>
       <FaGithub color="#eba417" />
       Sign In with GitHub
     </button>
